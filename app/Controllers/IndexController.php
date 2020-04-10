548ff1c6d7b50e0335bdfec1a4349fbdd3123db9
Javier Diaz <autopoietico@outlook.com>
2020-04-09 19:05:37 -0500

    Updated tier list with new data get in the fifth week of hero pools

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2020-03-27",
+        'LAST_DATA_UPDATE' => "2020-04-09",
         'COPY_DATE' => "2020"
     ];
 
