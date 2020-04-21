349158b7bcc641320f5f2765f1eadaa2a4a6fad7
Javier Diaz <autopoietico@outlook.com>
2020-04-21 09:44:17 -0500

    Updated counters, synergies and maps, updated tiers for week 7 of hero pools

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2020-04-20",
+        'LAST_DATA_UPDATE' => "2020-04-21",
         'COPY_DATE' => "2020"
     ];
 
