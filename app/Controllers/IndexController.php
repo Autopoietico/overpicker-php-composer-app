3c070ef53ae4a77123d6f400b0b18169606e11d0
Javier Diaz <autopoietico@outlook.com>
2020-08-12 06:56:23 -0500

    Update tier for second week of August, change counter between bastion and junkrat

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2020-07-29",
+        'LAST_DATA_UPDATE' => "2020-08-12",
         'COPY_DATE' => "2020"
     ];
 
