356a34cf932b6d0919c30c701619ac0bf0e333e6
Javier Diaz <autopoietico@outlook.com>
2020-08-18 06:57:05 -0500

    Update tier for third week of august, changed synergies for new offtank meta

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2020-08-12",
+        'LAST_DATA_UPDATE' => "2020-08-18",
         'COPY_DATE' => "2020"
     ];
 
