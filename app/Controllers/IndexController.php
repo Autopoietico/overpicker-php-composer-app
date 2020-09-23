f7ee2784c9d179cce49d88a09fa80f07fcc5c411
Javier Diaz <autopoietico@outlook.com>
2020-09-23 10:40:24 -0500

    Updated Hero Tiers for 4th week of September

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2020-09-16",
+        'LAST_DATA_UPDATE' => "2020-09-23",
         'COPY_DATE' => "2020"
     ];
 
