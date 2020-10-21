2d862d981a3bba7e2c0ad579f3ad6d768a66ae88
Javier Diaz <autopoietico@outlook.com>
2020-10-21 03:20:46 -0500

    Tiers updated for Third week of October

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2020-10-13",
+        'LAST_DATA_UPDATE' => "2020-10-21",
         'COPY_DATE' => "2020"
     ];
 
