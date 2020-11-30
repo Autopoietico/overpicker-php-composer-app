e7163664b71b76370cb3d86b9fccc47679fe84db
Javier Diaz <autopoietico@outlook.com>
2020-11-30 17:49:32 -0500

    Updated counter potential for certain dps heroes and mercy

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2020-11-28",
+        'LAST_DATA_UPDATE' => "2020-11-30",
         'COPY_DATE' => "2020"
     ];
 
