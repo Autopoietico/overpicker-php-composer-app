ff8c39f3435d497a760ad7de94e1f66288d08f93
Javier Diaz <autopoietico@outlook.com>
2020-12-12 01:30:15 -0500

    Updated counters for Brig and Echo

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2020-12-10",
+        'LAST_DATA_UPDATE' => "2020-12-12",
         'COPY_DATE' => "2020"
     ];
 
