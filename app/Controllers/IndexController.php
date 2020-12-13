755132c46be03ea357c0c02dd2e4ee71e89d9af0
Javier Diaz <autopoietico@outlook.com>
2020-12-13 04:05:58 -0500

    Updated sinergies for Sigma and Hog

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2020-12-12",
+        'LAST_DATA_UPDATE' => "2020-12-13",
         'COPY_DATE' => "2020"
     ];
 
