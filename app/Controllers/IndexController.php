1fcd4183fc0edd8af4a95fc8d734fb4218e4a7e0
Javier Diaz <autopoietico@outlook.com>
2020-12-20 07:57:31 -0500

    Update sinergies for Brig, Widow and Echo

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2020-12-18",
+        'LAST_DATA_UPDATE' => "2020-12-20",
         'COPY_DATE' => "2020"
     ];
 
