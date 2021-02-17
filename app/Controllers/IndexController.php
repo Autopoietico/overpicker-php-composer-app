13efb14e442ec465f3490d7b29409c80dd1795d3
Autopoietico <nizampop@gmail.com>
2021-02-17 04:26:41 -0500

    Updated tiers for third week of Febraury

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2021-02-14",
+        'LAST_DATA_UPDATE' => "2021-02-17",
         'COPY_DATE' => "2021"
     ];
 
