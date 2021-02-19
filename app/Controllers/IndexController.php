e5f10579c1de6a7cd27d3bd0d4ecf02b86ad6199
Autopoietico <nizampop@gmail.com>
2021-02-19 03:05:09 -0500

    Updated counters for Bastion, Brig, DVA, Doom, Echo, Genji, Hanzo, Junkrat, Lucio, Mccree, Moira, Orisa, Pharah, Reaper, Reinhardt, Sigma, Soldier, Symmetra, Widowmaker, Winston, Wrecking Ball, Zarya and Zenyatta

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2021-02-17",
+        'LAST_DATA_UPDATE' => "2021-02-19",
         'COPY_DATE' => "2021"
     ];
 
