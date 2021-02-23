32107bd7c13c210a691d580ebd9981df7b30131a
Autopoietico <nizampop@gmail.com>
2021-02-23 11:28:57 -0500

    Minor update of top 500 tiers

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2021-02-19",
+        'LAST_DATA_UPDATE' => "2021-02-23",
         'COPY_DATE' => "2021"
     ];
 
