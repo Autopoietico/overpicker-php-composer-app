e0b79a39ac0eec53e635f0757e44c4847433fa9c
Autopoietico <nizampop@gmail.com>
2021-03-08 19:22:42 -0500

    Changed the way the score for tiers are calculated

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2021-02-25",
+        'LAST_DATA_UPDATE' => "2021-03-08",
         'COPY_DATE' => "2021"
     ];
 
