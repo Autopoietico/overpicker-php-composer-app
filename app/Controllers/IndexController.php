626617b9659f84f368d919dafa0de40187f984bc
Autopoietico <autopoietico@outlook.com>
2021-11-17 22:51:40 -0500

    Updated counters for bastion

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2021-11-12",
+        'LAST_DATA_UPDATE' => "2021-11-17",
         'COPY_DATE' => "2021"
     ];
 
