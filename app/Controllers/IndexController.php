f4275288060392d3d42d91a38bc319b79faf9e57
Autopoietico <autopoietico@outlook.com>
2021-11-20 05:14:27 -0500

    Updated Orisa counters

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2021-11-19",
+        'LAST_DATA_UPDATE' => "2021-11-20",
         'COPY_DATE' => "2021"
     ];
 
