0a069e0f5cb48e60ab6a35116519d78033e461b7
Autopoietico <autopoietico@outlook.com>
2021-11-26 11:34:22 -0500

    Updated Mercy Counters

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2021-11-25",
+        'LAST_DATA_UPDATE' => "2021-11-26",
         'COPY_DATE' => "2021"
     ];
 
