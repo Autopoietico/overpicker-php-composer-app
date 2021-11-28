d4692b2e446ff3c721c2aa46609c6eb2a04ef2bd
Autopoietico <autopoietico@outlook.com>
2021-11-28 13:08:28 -0500

    Updated synergies for D.Va

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2021-11-26",
+        'LAST_DATA_UPDATE' => "2021-11-28",
         'COPY_DATE' => "2021"
     ];
 
