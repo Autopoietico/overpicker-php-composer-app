5479d404765e4e7f397ac20aaa6a720ef4650c4e
Autopoietico <autopoietico@outlook.com>
2021-11-30 14:17:12 -0500

    Updated Hog Synergies

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2021-11-28",
+        'LAST_DATA_UPDATE' => "2021-11-30",
         'COPY_DATE' => "2021"
     ];
 
