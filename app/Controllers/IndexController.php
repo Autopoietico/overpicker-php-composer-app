b9615c58eaa020853ba8718af8252337e5345b20
Autopoietico <autopoietico@outlook.com>
2021-12-03 00:05:59 -0500

    Updated tiers (2021-12-03)

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2021-11-30",
+        'LAST_DATA_UPDATE' => "2021-12-03",
         'COPY_DATE' => "2021"
     ];
 
