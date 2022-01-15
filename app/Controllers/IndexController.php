4c7390839b68be73f4b1cf74a26ad4fc98b27f51
Autopoietico <autopoietico@outlook.com>
2022-01-15 16:57:23 -0500

    Updated counters for Sigma

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController{
 
     public $DATES = [
 
-        'LAST_DATA_UPDATE' => "2021-01-13",
+        'LAST_DATA_UPDATE' => "2021-01-15",
         'COPY_DATE' => "2021"
     ];
 
